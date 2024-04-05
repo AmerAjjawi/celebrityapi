2caa9ed2c99acd4421b3288c6c508c36dc937019
Amer Ajjawi <ajjawi_amer@hotmail.com>
2024-04-05 17:03:07 -0400

    completed the resolve part of the promise to use later on

--- Form/index.js
+++ Form/index.js
@@ -1,3 +1,24 @@
+
+//function for validation
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //laying out the promise to be used later on
 const serverRequest = new Promise((resolve, reject => {
     let apiServerRequest = true;
@@ -6,5 +27,15 @@ const serverRequest = new Promise((resolve, reject => {
         serverRequest.then(result => {
             console.log(result);
         })
-    }
+    } // else statement will be there if the server can't be resolved
+      else {
+        reject ("coudln't fetch the data")
+        //telling server to catch the error
+        promise.apiServerRequest.catch(error)
+        mypromise.catch(error => {
+
+            //display the error
+            console.log(error); 
+        })
+      }
 }))
